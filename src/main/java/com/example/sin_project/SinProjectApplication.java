7dd44adff1e9691a425c8103dd71157fabc8bcb3
luciamalar <luciamalar98@gmail.com>
2022-10-11 09:57:26 +0200

    Initial commit

---
+++ src/main/java/com/example/sin_project/SinProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sin_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sin_project/SinProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sin_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
